3b66fcbaf9a9f2924d496b471782e860f070208c
AngelUche <ucheorji9974@gmail.com>
2023-03-21 19:32:42 +0000

    modified

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -3,24 +3,25 @@ import { DashboardHeader } from "./DashboardHeader";
 import { DashboardBody } from "./DashboardBody";
 
 function Dashboard() {
-
-    return (
-        <>
-            <div className="fixed top-0 bottom-[1.5rem] inset-x-0">
-                <div className="">
-                    <div className="mx-auto max-w-[700px]">
-                        <DashboardHeader />
-                    </div>
-                </div>
-
-                <div className="overflow-y-scroll">
-                    <div className="mx-auto max-w-[700px]">
-                        <DashboardBody />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div
+        className="top-0 inset-x-0 
+            flex-col mb-4"
+      >
+        {/* <div className=""> */}
+        <div className="mx-auto max-w-[600px]">
+          <DashboardHeader />
+        </div>
+        {/* </div> */}
+      </div>
+      <div className="overflow-y-scroll">
+        <div className="mx-auto max-w-[700px] h-[400px] pb-10">
+          <DashboardBody />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export { Dashboard };
--- src/components/dashboard/DashboardBody.tsx
+++ src/components/dashboard/DashboardBody.tsx
@@ -3,20 +3,26 @@ import { AllData } from "../../data/AllData";
 import { DateListView } from "./dateList";
 
 function DashboardBody() {
-    return (
-        <>
-            <div className="px-4 overflow-y-auto">
-                <div>
-                    <>
-                        {AllData.map((dateGroup) => {
-                            console.log(dateGroup);
-                            return <DateListView key={dateGroup.date} dateId={dateGroup.date} transactions={dateGroup.transactions} />
-                        })}
-                    </>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="px-4 overflow-y-aut">
+        <div>
+          <>
+            {AllData.map((dateGroup) => {
+              console.log(dateGroup);
+              return (
+                <DateListView
+                  key={dateGroup.date}
+                  dateId={dateGroup.date}
+                  transactions={dateGroup.transactions}
+                />
+              );
+            })}
+          </>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export { DashboardBody }
+export { DashboardBody };
